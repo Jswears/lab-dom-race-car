4f3eadb0c218efbe5ba6825be9c02c7d47eea98d
Jswears <ji.swearssalinas@gmail.com>
2023-06-22 17:35:54 +0200

    done

--- js/script.js
+++ js/script.js
@@ -1,12 +1,78 @@
-window.onload = function () {
+window.addEventListener("load", () => {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game;
+
+  function startGame() {
+    console.log("start game");
+    game = new Game();
+    game.start();
+    document.addEventListener("keydown", (event) => {
+      const key = event.key;
+      const possibleKeystrokes = [
+        "ArrowLeft",
+        "ArrowUp",
+        "ArrowRight",
+        "ArrowDown",
+      ];
+      if (possibleKeystrokes.includes(key)) {
+        // Update player's directionX and directionY based on the key pressed
+        switch (key) {
+          case "ArrowLeft":
+            game.player.directionX = -1;
+            break;
+          case "ArrowUp":
+            game.player.directionY = -1;
+            break;
+          case "ArrowRight":
+            game.player.directionX = 1;
+            break;
+          case "ArrowDown":
+            game.player.directionY = 1;
+            break;
+        }
+        console.log(game.player.directionX, game.player.directionY);
+      }
+    });
+    document.addEventListener("keyup", (event) => {
+      const key = event.key;
+      const possibleKeystrokes = [
+        "ArrowLeft",
+        "ArrowUp",
+        "ArrowRight",
+        "ArrowDown",
+      ];
+
+      if (possibleKeystrokes.includes(key)) {
+        // Update player's directionX and directionY based on the key pressed
+        switch (key) {
+          case "ArrowLeft":
+          case "ArrowRight":
+            game.player.directionX = 0;
+            break;
+          case "ArrowUp":
+          case "ArrowDown":
+            game.player.directionY = 0;
+            break;
+        }
+      }
+    });
+  }
 
   startButton.addEventListener("click", function () {
     startGame();
   });
 
-  function startGame() {
-    console.log("start game");
+  restartButton.addEventListener("click", () => {
+    location.reload();
+  });
+  restartButton.addEventListener("click", function () {
+    // Call the restartGame function when the button is clicked
+    restartGame();
+  });
+
+  // The function that reloads the page to start a new game
+  function restartGame() {
+    location.reload();
   }
-};
+});
